40cb93a92221b1780c0bac3c41deff47add2301c
lukew3 <lukew25073@gmail.com>
2020-01-02 13:20:37 -0500

    Working now

--- flask_portfolio/routes.py
+++ flask_portfolio/routes.py
@@ -2,15 +2,16 @@ from flask import render_template, url_for, flash, redirect
 from flask_portfolio import app
 from flask_portfolio.models import Example
 
+
 @app.route("/")
 @app.route("/home")
 def home():
-    examples = Example.query.all()
-    return render_template('home.html', examples=examples)
+    return render_template('home.html')
 
 @app.route("/examples")
 def examples():
-    return render_template('examples.html')
+    examples = Example.query.all()
+    return render_template('examples.html', examples=examples)
 
 @app.route("/contact")
 def contact():
