f8972bc2fae4eed9fce8addadfffd47ae50d0e16
lukew3 <lukew25073@gmail.com>
2020-01-01 22:30:29 -0500

    database created, is still not being read by examples.html

--- flask_portfolio/__init__.py
+++ flask_portfolio/__init__.py
@@ -4,6 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)    
 
 from flask_portfolio import routes
--- flask_portfolio/models.py
+++ flask_portfolio/models.py
@@ -1,11 +1,11 @@
 from flask_portfolio import db
 
-class Example(db.Model)
-    id = db.column(db.Integer, primary_key=True)
+class Example(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), unique=True, nullable=False)
     date_started = db.Column(db.String(11), unique=False, nullable=False)
     description = db.Column(db.String(1000), unique=True, nullable=False)
-    github_link = db.Colummn(db.String(200), unique=True, nullable=True)
+    github_link = db.Column(db.String(200), unique=True, nullable=True)
     live_link = db.Column(db.String(200), unique=True, nullable=True)
 
     def __repr__(self):
